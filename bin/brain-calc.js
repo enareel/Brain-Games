77d37077022bff24e14f32ae21922c77191f5cc8
Nikolay Moryakin <moryakinn@gmail.com>
2021-06-28 15:23:20 +0300

    Линтинг

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,32 +1,20 @@
 #!/usr/bin/env node
 /**
- * Программа (скрипт) калькулятора
+ * Программа (скрипт) проверки числа на четность
  */
-import readlineSync from 'readline-sync';
-import cli from '../src/cli.js';
+import { showRandomNumber, getKeyByValue } from '../src/utils.js';
+import engine from '../src/engine.js';
 
 // Вспомогательные переменные
-const Message = {
-  GREETINGS: 'Welcome to the Brain Games!',
-  RULE: 'Answer "yes" if the number is even, otherwise answer "no".',
-};
-const MagicNumber = {
-  TIMES: 3,
-  LEFT: 1,
-  RIGHT: 10,
+const ruleMsg = 'Answer "yes" if the number is even, otherwise answer "no".';
+const Limit = {
+  LEFT: 5,
+  RIGHT: 25,
 };
 const answerMap = {
   no: false,
   yes: true,
 };
-
-/**
- * Функция возврата ключа объекта по значению
- * @param {Object} obj
- * @param {*} value
- * @returns {String}
- */
-const getKeyByValue = (obj, value) => Object.keys(obj).find((key) => obj[key] === value);
 
 /**
  * Функция проверки числа на четность
@@ -36,43 +24,22 @@ const getKeyByValue = (obj, value) => Object.keys(obj).find((key) => obj[key] ==
 const isEven = (num) => !(num % 2) && num > 0;
 
 /**
- * Функция вывода целого рандомного числа
- * в заданных границах
- * @param {Number} a
- * @param {Number} b
- * @returns {Number}
+ * Основная функция
+ * @returns {Object}
  */
-const showRandomNumber = (a, b) => Math.floor(Math.random() * (b - a + 1) + a);
+const evenGame = () => {
+  // Получаем рандомное число
+  const randomNumber = showRandomNumber(Limit.LEFT, Limit.RIGHT);
 
-/**
- * Функция осуществления игры
- * @param {String} name
- * @returns {Number}
- */
-const startGame = (name) => {
-  let finalResult = `Congratulations, ${name}!`;
-  for (let i = 0; i < MagicNumber.TIMES; i += 1) {
-    const tmpValue = showRandomNumber(MagicNumber.LEFT, MagicNumber.RIGHT);
-    console.log(`Question: ${tmpValue}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    const correctAnswer = getKeyByValue(answerMap, isEven(tmpValue));
-    if (correctAnswer === userAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
-      finalResult = `Let's try again, ${name}!`;
-      break;
-    }
-  }
-  console.log(finalResult);
-  return finalResult;
+  // Получаем правильный ответ на вопрос
+  const answer = getKeyByValue(answerMap, isEven(randomNumber));
+
+  return {
+    answer,
+    value: randomNumber,
+    expression: randomNumber,
+  };
 };
 
-// Приветствуем пользователя и описываем правила игры
-console.log(`${Message.GREETINGS}`);
-const userName = cli();
-console.log(`${Message.RULE}`);
-
 // Старт игры
-startGame(userName);
-ls;
+engine(ruleMsg, evenGame);
